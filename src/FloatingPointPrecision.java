f65a03c6dc3d1cb7ece61044324a656d840e70f4
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-19 19:13:38 +0530

    float vs double

---
+++ src/FloatingPointPrecision.java
@@ -0,0 +1,9 @@
+public class FloatingPointPrecision {
+    public static void main (String[] args){
+        float minFloat = Float.MIN_VALUE;
+        System.out.println(minFloat);
+
+        double minDouble = Double.MIN_VALUE;
+        System.out.println(minDouble);
+    }
+}
