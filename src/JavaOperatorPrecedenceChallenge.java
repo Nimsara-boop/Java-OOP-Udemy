b403730b1b75d886252386660d43e0e2e5ad922a
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-19 22:26:44 +0530

    did some operation work

---
+++ src/JavaOperatorPrecedenceChallenge.java
@@ -0,0 +1,18 @@
+public class JavaOperatorPrecedenceChallenge {
+    public static void main (String[] args){
+        double value1 = 20.00;
+        double value2 = 80.00;
+
+        double sum1 = 100.00*(value1 + value2);
+
+        double remainder = sum1 % 40.00;
+        System.out.println(remainder);
+
+        boolean check = remainder==0.00? true : false;
+        System.out.println(check);
+
+        if (!check){
+            System.out.println("Got some remainder!");
+        }
+    }
+}
