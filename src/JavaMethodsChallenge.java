654687cf86352772facc6183b80333f84af42219
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-25 15:50:54 +0530

    scores with HASHMAP

--- src/JavaMethodsChallenge.java
+++ src/JavaMethodsChallenge.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaMethodsChallenge {
 
@@ -8,17 +10,21 @@ public class JavaMethodsChallenge {
         else if (500>score && score>=100) return 3;
         else return 4;
     }
-    public void displayHighScorePosition(int position){
-        System.out.println("managed to get into position "+position+" on the score list");
+    public void displayHighScorePosition(String name, int position){
+        System.out.println(name+" managed to get into position "+position+" on the score list");
     }
 
     public static void main(String[] args) {
+
         JavaMethodsChallenge challenge = new JavaMethodsChallenge();
-        ArrayList<Integer> scoresList = new ArrayList<>();
-        scoresList.add(1500); scoresList.add(1000); scoresList.add(500); scoresList.add(100); scoresList.add(25);
-        for (int score : scoresList){
-            int position = challenge.calculateHighScorePosition(score);
-            challenge.displayHighScorePosition(position);
+
+        //ArrayList<Integer> scoresList = new ArrayList<>();
+        Map<String, Integer> scoresList = new HashMap<String, Integer>();
+        scoresList.put("Melody", 2500); scoresList.put("Tim", 1500); scoresList.put("Jonah", 1000); scoresList.put("Marley", 500); scoresList.put("Yennifer", 100); scoresList.put("Alex",25);
+
+        for (Map.Entry<String, Integer> entry : scoresList.entrySet()){
+            int position = challenge.calculateHighScorePosition(entry.getValue());
+            challenge.displayHighScorePosition(entry.getKey(), position);
         }
 
     }
