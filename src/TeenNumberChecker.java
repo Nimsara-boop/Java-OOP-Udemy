6933aca7145ccc83a730f358374882162af514c1
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-26 20:01:12 +0530

    The method should return boolean and it needs to return true if one of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,8 @@
+public class TeenNumberChecker {
+    public static boolean hasTeen(int a , int b, int c){
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
+    public static boolean isTeen(int d){
+        return (d>=13 && d<=19);
+    }
+}
