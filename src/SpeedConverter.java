a0f2ffd4f4365170e87ed7d066b4c8a2c098c474
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-26 11:42:31 +0530

    speed converter. used Math.round to get expected round values

---
+++ src/SpeedConverter.java
@@ -0,0 +1,12 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour){
+        long milesPerHour = 0;
+        return (kilometersPerHour<0) ? -1 :
+        (milesPerHour = Math.round(kilometersPerHour/1.609));
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        int kilometerPerHour = 0;
+        System.out.println( (kilometersPerHour<0) ? "Invalid Value" : kilometersPerHour+" km/h = "+toMilesPerHour(kilometersPerHour)+"mi/h");
+    }
+}
