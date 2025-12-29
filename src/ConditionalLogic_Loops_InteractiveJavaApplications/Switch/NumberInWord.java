b14ad37b53ab95ec7e9b92d14ed0d003349e992f
Pugsy <nimsarakarunaratne@gmail.com>
2025-12-29 21:52:00 +0530

    easy switch exercise

--- src/ConditionalLogic_Loops_InteractiveJavaApplications/Switch/NumberInWord.java
+++ src/ConditionalLogic_Loops_InteractiveJavaApplications/Switch/NumberInWord.java
@@ -1,4 +1,16 @@
 package ConditionalLogic_Loops_InteractiveJavaApplications.Switch;
 
 public class NumberInWord {
+    public static void printNumberInWord(int number){
+        String word = switch(number){
+            case 0 -> "ZERO"; case 6 -> "SIX";
+            case 1-> "ONE"; case 7->"SEVEN";
+            case 2 -> "TWO"; case 8-> "EIGHT";
+            case 3 -> "THREE"; case 9 -> "NINE";
+            case 4 ->"FOUR";
+            case 5 -> "FIVE";
+            default ->"OTHER";
+        };
+        System.out.println(word);
+    }
 }
